5f93504fa0239ab5c437ae8898fe1bcca0eb68bc
Joseph Spinks <jms2895@outlook.com>
2023-05-02 20:30:05 +0100

    seeded file, created routes and controller actions for restaurant index, show, and new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+super_pizza = {name: "Super Pizza", address: "69 made up Lane, London, E2 C87", category: "italian" }
+silk_road = {name: "Silk Road", address: "7 Camberwell Lane, London SE4 S21", category: "chinese" }
+ramen_man =  {name: "Ramen Man", address: "76A  High St, London E9 6PQ", category: "japanese" }
+waffle_house = {name: "Waffle House", address: "100  Low St, London N2 6PQ", category: "belgian" }
+
+[dishoom, pizza_east, super_pizza, silk_road, ramen_man, waffle_house].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "fin!"
